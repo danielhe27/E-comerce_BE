8926116db46e7eec3ca153ed21360d781f447fc6
danielhe27 <jdah49d@gmail.com>
2024-03-18 18:04:43 -0400

    import connections

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const routes = require('./routes');
+
 // import sequelize connection
+const Category = require('./models/Category');
+const Product = require('./models/Product');
+const Tag = require('./models/Tag');
+const ProductTag = require('./models/ProductTag');
 
 //import the connection object
 const sequelize = require('./config/connection');
